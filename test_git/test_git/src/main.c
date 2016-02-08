621e08973d91b2003f8dabed1fbaedd4709b86a0
reza karbasi <a.rz.karbasi@gmail.com>
2016-02-08 14:09:06 +0330

    second change
    second change

--- test_git/test_git/src/main.c
+++ test_git/test_git/src/main.c
@@ -33,7 +33,8 @@
 int main (void)
 {
 	/* Insert system clock initialization code here (sysclk_init()). */
-
+	
+	sysclk_init();
 	board_init();
 
 	/* Insert application code here, after the board has been initialized. */
